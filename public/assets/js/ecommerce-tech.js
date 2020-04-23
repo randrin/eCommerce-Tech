4e1b96366a950a1183a8357ffbb19004d759aea6
@nzeukangnimpar <nzeukangrandrin@gmail.com>
2020-04-23 22:55:01 +0200

    Fixed scroll header

--- public/assets/js/ecommerce-tech.js
+++ public/assets/js/ecommerce-tech.js
@@ -8,4 +8,21 @@ $(document).ready(function () {
   $notificationClose.on("click", function (e) {
     $(this).parent().parent().parent().css("display", "none");
   });
+
+  // Disable initial effet scroll
+  var $initialScroll = $(".sticky-header");
+  var $newScroll = $(".header-middle");
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 380) {
+      if ($initialScroll.length && $(window).width() >= 992) {
+        $initialScroll.addClass("stop-fixed");
+      }
+      if ($newScroll.length && $(window).width() >= 992) {
+        $newScroll.addClass("new-fixed");
+      }
+    } else {
+      $initialScroll.removeClass("stop-fixed");
+      $newScroll.removeClass("new-fixed");
+    }
+  });
 });
