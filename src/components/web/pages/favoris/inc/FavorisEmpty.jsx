eca76e7d947aab16428d813dd35d5aa97a4ea065
@nzeukangnimpar <nzeukangrandrin@gmail.com>
2020-07-12 10:10:14 +0200

    Favories Empty Steps

--- src/components/web/pages/favoris/inc/FavorisEmpty.jsx
+++ src/components/web/pages/favoris/inc/FavorisEmpty.jsx
@@ -7,22 +7,64 @@ export default class FavorisEmpty extends Component {
       <Translation>
         {(t) => (
           <>
-            <div className="container">
+            <div className="store-favories-wrapper container">
               <div className="page-content text-center">
-                <h2 className="title">Oufff !!!!</h2>
-                <p>{t("WISHLIST.TEXT")}</p>
-                <div className="row m-5">
-                  <div className="col-md-6">
-                    <Link to="/login" class="btn btn-outline-primary-2">
-                      <span>{t("LOGIN.BREADCRUMB")}</span>
-                      <i class="icon-long-arrow-right"></i>
-                    </Link>
+                <div className="store-favories-sleep">
+                  <img
+                    src="/assets/images/Favories-sleep.png"
+                    alt="Faories Sleep"
+                  />
+                </div>
+                <div className="row">
+                  <div className="col-md-8 mr-auto ml-auto">
+                    <p>{t("WISHLIST.STEP_1")}</p>
+                    <hr />
+                    <p>{t("WISHLIST.STEP_2")}</p>
+                    <div className="store-favories-steps mt-4">
+                      <span className="btn-product-icon btn-wishlist"></span>
+                      <span className="mr-5 ml-5">
+                        <i className="icon-long-arrow-right"></i>
+                      </span>
+                      <span className="btn-product-icon btn-wishlist-selected"></span>
+                    </div>
+                    <p className="mt-3">{t("WISHLIST.STEP_3")}</p>
                   </div>
-                  <div className="col-md-6 store-favoris">
-                    <Link to="/register" class="btn btn-primary btn-g">
-                      <span>{t("LOGOUT.BREADCRUMB")}</span>
-                      <i class="icon-long-arrow-right"></i>
-                    </Link>
+                </div>
+
+                {/* <p>{t("WISHLIST.TEXT")}</p> */}
+
+                <div className="row justify-content-center mt-5">
+                  <div className="col-lg-6 col-sm-6">
+                    <div className="icon-box icon-box-circle text-center">
+                      <span className="icon-box-icon">
+                        <i className="fa fa-smile-o"></i>
+                      </span>
+                      <div className="icon-box-content">
+                        <h3 className="icon-box-title mb-2">
+                          {t("WISHLIST.TEXT_ALREADY_USER")}
+                        </h3>
+                      </div>
+                      <Link to="/login" className="btn btn-outline-primary-2">
+                        <span>{t("LOGIN.BREADCRUMB")}</span>
+                        <i className="icon-long-arrow-right"></i>
+                      </Link>
+                    </div>
+                  </div>
+                  <div className="col-lg-6 col-sm-6">
+                    <div className="icon-box icon-box-circle text-center store-favoris">
+                      <span className="icon-box-icon">
+                        <i className="fa fa-frown-o"></i>
+                      </span>
+                      <div className="icon-box-content">
+                        <h3 className="icon-box-title mb-2">
+                          {t("WISHLIST.TEXT_NEW_USER")}
+                        </h3>
+                      </div>
+                      <Link to="/register" className="btn btn-primary btn-g">
+                        <span>{t("LOGIN.CREATE_ACCOUNT")}</span>
+                        <i className="icon-long-arrow-right"></i>
+                      </Link>
+                    </div>
                   </div>
                 </div>
               </div>
